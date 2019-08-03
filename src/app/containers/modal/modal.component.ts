b7041208b2bc822532be897ed89b9eac4b32f06e
akashdhar.apssdc@gmail.com <Vasanta@123#>
2019-08-04 03:13:36 +0530

    modal popup changes

--- src/app/containers/modal/modal.component.ts
+++ src/app/containers/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'alpha-modal',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  redirectToAvailOffers(){
+    //this.router.navigate(['/alpha-ola-offers']);
   }
 
 }

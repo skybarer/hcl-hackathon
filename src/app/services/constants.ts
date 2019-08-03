7c91266ea56b36ea4d0ff1b2136cc33c8046c3bb
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-08-03 18:34:24 +0530

    added constants and api service

---
+++ src/app/services/constants.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Constants {
+   IPADDRESS = 'http://192.168.1.100';
+   PORT = '8082';
+   URI = 'http://192.168.1.100:8082/';
+   LOGIN = 'login';
+   DASHBOARD = 'balances?proxyId=';
+}

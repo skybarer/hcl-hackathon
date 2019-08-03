e341219b26f1942265f3148cb8a0c927ad92d38c
akashdhar.apssdc@gmail.com <Vasanta@123#>
2019-08-03 18:48:10 +0530

    login form service init commit.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'alpha-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm: FormGroup;
+  isLoading = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+
   }
 
 }

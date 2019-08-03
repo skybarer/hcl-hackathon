d2b61e91c5c56744e09302b52662abd0b47b3e15
akashdhar.apssdc@gmail.com <Vasanta@123#>
2019-08-04 01:44:27 +0530

    UI Changes.

--- src/app/containers/upi-password/upi-password.component.ts
+++ src/app/containers/upi-password/upi-password.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {MatDialog, MatSnackBar} from '@angular/material';
 import {ModalComponent} from '../modal/modal.component';
 
@@ -14,9 +14,11 @@ export class UpiPasswordComponent implements OnInit {
     private router: Router,
     private dialog: MatDialog,
     private snackBar: MatSnackBar
-  ) { }
+  ) {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
 
   onSubmit() {
     const dialogRef = this.dialog.open(ModalComponent, {

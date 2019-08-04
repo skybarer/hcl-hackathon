363a46293399e9d64ec009243aa4c65ac4fa85ac
akashdhar.apssdc@gmail.com <Vasanta@123#>
2019-08-04 09:16:15 +0530

    Corrected typo

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class Constants {
-   URI = 'http://10.0.2.107:8082/';
+   URI = 'http://192.168.1.100:8082/';
    DASHBOARD = 'balances?proxyId=';
    WALLET_TRANSFER = 'transactions/settlement';
 }
